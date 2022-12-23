5940ee13b6dda0a6a8f81b7dba8d70053b1a9906
luaxx <100285417+1uaxx@users.noreply.github.com>
2022-12-22 19:36:43 -0600

    Create lorisoppersearcher.lua

---
+++ lorisoppersearcher.lua
@@ -0,0 +1,17 @@
+local Opps = {
+    "tayex",
+    "lapt",
+    "federal",
+    "encode",
+    "UnorthodoxJason"
+}
+
+function FuckOpps()
+    for _, v in pairs(Opps) do
+        if game.Players.LocalPlayer.Name == v then
+            game.Players.LocalPlayer:Kick("Suck my penis - loris")
+        end
+    end
+end
+
+FuckOpps()
